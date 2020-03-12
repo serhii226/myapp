d8068a5d355379fd11949b73fc11c07fb3739eb5
serhii226 <myzsoftdev1@gmail.com>
2020-03-12 04:25:46 +0200

    todo model

--- src/App.js
+++ src/App.js
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Amplify, {
+  API,
+  graphqlOperation
+} from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      id
+      name
+      description
+    }
+  }
+}`;
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`;
+
+class App extends React.Component {
+  todoMutation = async () => {
+    const todoDetails = {
+      name: 'Party tonight!',
+      description: 'Amplify CLI rocks!'
+    };
+  
+    const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
+    alert(JSON.stringify(newTodo));
+  };
+  
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <p> Pick a file</p>
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const signUpConfig = {
+  header: "Sign Up",
+  hideAllDefaults: true,
+  defaultCountryCode: "1",
+  signUpFields: [
+    {
+      label: "My user name",
+      key: "username",
+      required: true,
+      displayOrder: 1,
+      type: "string"
+    },
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 4,
+      type: "string"
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 2,
+      type: "password"
+    }
+  ]
+};
+
+const usernameAttributes = "Email";
+
+export default withAuthenticator(App, { signUpConfig, usernameAttributes });
